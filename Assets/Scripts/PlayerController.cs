03fc6ba9db08d03f0f22fd15e681420cca3ecbb6
Leo88p <71487423+Leo88p@users.noreply.github.com>
2026-02-18 13:58:36 +0300

    Fixed rope

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,17 +13,22 @@ public class PlayerController : MonoBehaviour
     {
         _inputManager = GetComponent<InputManager>();
         _rb = GetComponent<Rigidbody>();
-        EventManager.OnRopeTriggered += RopeTriggered;
-        EventManager.OnRopeExit += RopeExit;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         Vector2 rotation = _inputManager.GetRotateAction();
-        transform.Rotate(0, rotation.x, 0);
+        if (!isOnRope)
+        {
+            transform.Rotate(0, rotation.x, 0);
+        }
         MainCamera.transform.Rotate(-rotation.y, 0, 0);
         Vector2 moveAction = _inputManager.GetMoveAction();
+        if (isOnRope)
+        {
+            IsStillOnRope();
+        }
         if (!isOnRope)
         {
             Vector3 moveDirection = new Vector3(moveAction.x, 0, moveAction.y);
@@ -33,7 +38,7 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-            _rb.linearVelocity = 0.5f * moveAction.y * Vector3.up;
+            _rb.linearVelocity += 0.5f * moveAction.y * Vector3.up;
         }
         if (_inputManager.GetJumpAction() > 0)
         {
@@ -43,7 +48,7 @@ public class PlayerController : MonoBehaviour
             }
             else if (isOnRope)
             {
-                _rb.AddForce(5 * transform.forward, ForceMode.Impulse);
+                //_rb.AddForce(5 * transform.forward, ForceMode.Impulse);
             }
         }
     }
@@ -54,24 +59,25 @@ public class PlayerController : MonoBehaviour
             if (Vector3.Dot(contact.normal, Vector3.up) > 0.75f)
             {
                 isOnGround = true;
+                isOnRope = false;
+                _rb.useGravity = true;
                 return;
             }
         }
     }
-    void OnCollisionEnter(Collision collision)
+    void RopeCheck(Collision collision)
     {
-        GroundCheck(collision);
-    }
-    void OnCollisionStay(Collision collision)
-    {
-        GroundCheck(collision);
-    }
-    void OnCollisionExit(Collision collision)
-    {
-        isOnGround = false;
-    }
-    void RopeTriggered()
-    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            if (Vector3.Dot(contact.normal, -transform.forward) >0.85f)
+            {
+                break;
+            }
+            else
+            {
+                return;
+            }
+        }
         if (!isOnGround)
         {
             isOnRope = true;
@@ -84,9 +90,35 @@ public class PlayerController : MonoBehaviour
             _rb.useGravity = true;
         }
     }
-    void RopeExit()
+    void IsStillOnRope()
     {
-        isOnRope = false;
-        _rb.useGravity = true;
+        if (Physics.CapsuleCast(transform.position + 0.125f * Vector3.up, transform.position - 0.125f * Vector3.up,
+            0.25f, transform.forward, out RaycastHit hit, 0.25f, LayerMask.GetMask("Rope")))
+        {
+            _rb.linearVelocity = Vector3.zero;
+            _rb.linearVelocity += hit.distance * transform.forward;
+        }
+        else
+        {
+            isOnRope = false;
+            _rb.useGravity = true;
+            _rb.linearVelocity = Vector3.zero;
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Rope"))
+            RopeCheck(collision);
+        GroundCheck(collision);
+    }
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Rope"))
+            RopeCheck(collision);
+        GroundCheck(collision);
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        isOnGround = false;
     }
 }
